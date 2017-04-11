aa33d77c5e2a268af04617fab5a17ca64c373de2
喻希里 <293730698@qq.com>
2017-04-11 10:59:00 +0800

    去重（数组对象）

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 require("path");
-
+function unique(arr){
+		var obj = {};
+		var objArr = [];
+		var tempArr = [];
+		for(var i = 0;i<arr.length;i++){
+			if(!(arr[i] instanceof Object)){
+				if(!obj[arr[i]]){
+					tempArr.push(arr[i]);
+					obj[arr[i]] = true;
+				}
+			}else{
+				if(objArr.indexOf(arr[i])==-1){
+					objArr.push(arr[i]);
+					tempArr.push(arr[i]);
+				}
+			}
+		}
+		return tempArr;
+	}
+	var b = {b:1};
+	var arr = [b,{a:1},{a:1},b,b]
+	
+	console.log(unique(arr));
 console.log("helow world hello world");
